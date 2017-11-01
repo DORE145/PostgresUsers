1b537e9bd54b2faa571150deecfb7e28891a7683
Michael Kuzhel <michael.kuzhel@gmail.com>
2017-11-01 15:52:49 +0300

    Create User bean

---
+++ src/com/dore/postgesUsers/beans/User.java
@@ -0,0 +1,56 @@
+package com.dore.postgesUsers.beans;
+
+import java.util.Date;
+
+public class User {
+
+	private int id;
+	private String name;
+	private String surname;
+	private String login;
+	private String password;
+	private Date birthday;
+	
+	public User() {
+		
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSurname() {
+		return surname;
+	}
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Date getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	} 
+	
+	
+}
